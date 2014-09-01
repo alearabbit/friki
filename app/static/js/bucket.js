7017b5e55af9dd0e5e7d3580d2a640a43dc8e761
Seungho Jeong <denovator@outlook.com>
2014-09-02 03:02:46 +0900

    main_bucket.html 레이아웃 아이콘 드롭다운 구현 1단계

--- app/static/js/bucket.js
+++ app/static/js/bucket.js
@@ -1,5 +1,5 @@
 $(document).ready(function (){
-    $('.dropdown-toggle').dropdown()
+    $('.dropdown-toggle').dropdown() // 드롭다운 작동을 위한 라인 1개
 
     pusher = new Pusher('dd2181a309fb7773ac46');
     
@@ -11,7 +11,7 @@ $(document).ready(function (){
 
     my_channel.bind('new_msg', function(data){
         $('#dropdown').append('<img src="http://graph.facebook.com/'+ data.user_id + '/picture">');
-       
+
     // 위 val() 코드는 text입력 후 기존에 입력한 내용을 지움.(빈 박스로)
-    })          
+})          
 })
